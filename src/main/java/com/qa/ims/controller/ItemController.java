2b23c91be3b0ad6145a97eaa094aee1eb1bc8a1f
Owen <OJackson@qa.com>
2020-09-14 14:56:34 +0100

    filling item folders using customer template

--- src/main/java/com/qa/ims/controller/ItemController.java
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -1,5 +1,79 @@
 package com.qa.ims.controller;
 
-public class ItemController {
+import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.CustomerDAO;
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Customer;
+import com.qa.ims.utils.Utils;
+
+public class ItemController implements CrudController<Item> {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private ItemDAO itemDAO;
+	private Utils utils;
+
+	public ItemController(ItemDAO itemDAO, Utils utils) {
+		super();
+		this.itemDAO = itemDAO;
+		this.utils = utils;
+	}
+
+	/**
+	 * Reads all customers to the logger
+	 */
+	@Override
+	public List<Item> readAll() {
+		List<Item> items = itemsDAO.readAll();
+		for (Items items : items) {
+			LOGGER.info(items.toString());
+		}
+		return items;
+	}
+
+	/**
+	 * Creates a Item by taking in user input
+	 */
+	@Override
+	public Items create() {
+		LOGGER.info("Name of Item");
+		String itemName = utils.getString();
+		LOGGER.info("Please enter value of Item");
+		float value = utils.getString();
+		Item item = itemDAO.create(new Item(itemName, value));
+		LOGGER.info("Item created");
+		return item;
+	}
+
+	/**
+	 * Updates an existing Item by taking in user input
+	 */
+	@Override
+	public Items update() {
+		LOGGER.info("Please enter the id of the Item you would like to update");
+		Long id = utils.getLong();
+		LOGGER.info("Please enter Item Name");
+		String itemName = utils.getString();
+		LOGGER.info("Please enter value of Item");
+		float value = utils.getString();
+		Item item = itemsDAO.update(new Item(id, itemName, value));
+		LOGGER.info("Item Updated");
+		return item;
+	}
+
+	/**
+	 * Deletes an existing Item by the id of the Item
+	 * 
+	 * @return
+	 */
+	@Override
+	public int delete() {
+		LOGGER.info("Please enter the id of the Item you would like to delete");
+		Long id = utils.getLong();
+		return itemsDAO.delete(id);
+	}
 }
--- src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -1,5 +1,133 @@
 package com.qa.ims.persistence.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.DBUtils;
+
 public class ItemDAO {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+		Long id = resultSet.getLong("id");
+		String itemName = resultSet.getString("item_name");
+		float value = resultSet.getString("value");
+		return new Item(id, itemName, value);
+	}
+
+	/**
+	 * Reads all items from the database
+	 * 
+	 * @return A list of items
+	 */
+	@Override
+	public List<Items> readAll() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("select * from items");) {
+			List<Items> items = new ArrayList<>();
+			while (resultSet.next()) {
+				items.add(modelFromResultSet(resultSet));
+			}
+			return items;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+	}
+
+	public Items readLatest() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM items ORDER BY id DESC LIMIT 1");) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Creates a item in the database
+	 * 
+	 * @param item - takes in a item object. id will be ignored
+	 */
+	@Override
+	public Item create(Item item) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("INSERT INTO customers(item_name, value) values('" + item.getItemName()
+					+ "','" + item.getValue() + "')");
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	public Item readItem(Long id) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM items where id = " + id);) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Updates a customer in the database
+	 * 
+	 * @param customer - takes in a customer object, the id field will be used to
+	 *                 update that customer in the database
+	 * @return
+	 */
+	@Override
+	public Customer update(Customer customer) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("update customers set first_name ='" + customer.getFirstName() + "', surname ='"
+					+ customer.getSurname() + "' where id =" + customer.getId());
+			return readCustomer(customer.getId());
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Deletes a customer in the database
+	 * 
+	 * @param id - id of the customer
+	 */
+	@Override
+	public int delete(long id) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();) {
+			return statement.executeUpdate("delete from customers where id = " + id);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return 0;
+	}
 
 }
